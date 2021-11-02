aa3b3fe99862815b99bf483105147fb68520d4f5
Nurbek766 <79398064+Nurbek766@users.noreply.github.com>
2021-11-02 19:30:42 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function getCheckboxValuesForLoop(html_collection){
+    let price = 0;
+    for (let i = 0; i < html_collection.length; i++) {
+        if (html_collection[i].checked) {
+            price = price + Number(html_collection[i].value)
+        }
+    }
+    return price;
+}
+function calculate(){
+
+    let price = Number(document.getElementById("cost").value);
+
+    let name = document.getElementById("nam").value;
+
+    var education = Number(document.getElementById("education").value);
+    price *= education;
+    console.log(price);//+
+    
+    var networth = Number(document.getElementById("networth").value);
+    price *= networth;
+    console.log(price);//*
+
+    var caste = Number(document.getElementById("caste").value);
+    price += caste;
+    console.log(price);//+
+
+    var skills = document.getElementsByName("myCheck");
+    price += getCheckboxValuesForLoop(skills);
+    console.log(price);
+
+    var age = Number(document.getElementById("myAge").value);
+    price *= age;
+    console.log(price);
+
+    var reputation = document.getElementsByName("myReputation");
+    price += getCheckboxValuesForLoop(reputation);
+    
+
+    const text = document.getElementById("text");
+
+
+    document.getElementById("submit").innerHTML = `The price for your bride is ${nam}.,${price}.,${text}.`;
+    console.log(price);
+}
